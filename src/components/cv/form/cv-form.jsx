218244762395f008a9edec35b276eee5787a6b99
JFAldridge <jfaldridge1985@gmail.com>
2021-07-29 10:42:19 -0400

    Extract CVInput into it's own module

--- src/components/cv/form/cv-form.jsx
+++ src/components/cv/form/cv-form.jsx
@@ -1,46 +1,5 @@
 import React, { Component } from 'react';
-
-function CVInput( {fieldName, currentValue, inputChangeHandle} ) {
-    
-    const inputType = (name) => {
-        if (name === 'tel' || name === 'email') {
-            return name
-        } else {
-            return 'text';
-        }
-    }
-
-    const capitalize = (string) => {
-        return string.charAt(0).toUpperCase() + string.slice(1);
-    }
-
-    const handleInputChange = (event) => {
-        inputChangeHandle(event);
-    }
-
-
-    return (
-        <div className="row g-3 align-items-center">
-            <div className="col-auto">
-                <label 
-                    htmlFor={fieldName} 
-                    className="form-label">
-                {capitalize(fieldName)}
-                </label>
-            </div>
-            <div className="col-auto">
-                <input 
-                    type={inputType(fieldName)} 
-                    className="form-control" 
-                    id={fieldName}
-                    name={fieldName}
-                    value={currentValue} 
-                    onChange={handleInputChange}
-                />
-            </div>
-        </div>
-    );
-}
+import CVInput from './cv-input';
 
 class CVForm extends Component {
     constructor(props) {
