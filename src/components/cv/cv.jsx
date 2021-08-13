9bfc351b337a397b303a1e8a2096911cd6ff1bdd
JFAldridge <jfaldridge1985@gmail.com>
2021-08-13 16:27:45 -0400

    Shift CV styling from SCSS into styled-component

--- src/components/cv/cv.jsx
+++ src/components/cv/cv.jsx
@@ -5,44 +5,56 @@ import Education from './display/education';
 import Skills from './display/skills';
 import Introduction from './display/introduction';
 import WorkExperience from './display/work-experience';
+import styled from 'styled-components';
 
 //react-to-print only prints class components
+
+const CVWrapper = styled.div`
+	word-wrap: break-word;
+    display: flex;
+    width: 8.5in;
+    height: 11in;
+    background-color: rgb(248, 248, 248);
+    color: $rgb(24, 24, 24);
+    font-family: 'Open Sans', sans-serif;
+    text-align: left;
+`;
 
 class CV extends React.Component {
 	render() {
 		const {portrait, contact, education, skills, introduction, workExperience, formDisplay} = this.props;
 
 		return (
-			<div className="resume">
-				<div className="sidebar">
-					<Portrait 
-						fields={portrait}
-						formDisplay={formDisplay}
-					/>
-					<Contact 
-						fields={contact}
-						formDisplay={formDisplay}
-					/>
-					<Education 
-						fields={education}
-						formDisplay={formDisplay}
-					/>
-					<Skills 
-						fields={skills}
-						formDisplay={formDisplay}
-					/>
-				</div>
-				<div className="main-content">
-					<Introduction 
-						fields={introduction}
-						formDisplay={formDisplay}
-					/>
-					<WorkExperience 
-						fields={workExperience}
-						formDisplay={formDisplay}
-					/>
-				</div>
-			</div>
+			<CVWrapper className="cv">
+					<div className="sidebar">
+						<Portrait 
+							fields={portrait}
+							formDisplay={formDisplay}
+						/>
+						<Contact 
+							fields={contact}
+							formDisplay={formDisplay}
+						/>
+						<Education 
+							fields={education}
+							formDisplay={formDisplay}
+						/>
+						<Skills 
+							fields={skills}
+							formDisplay={formDisplay}
+						/>
+					</div>
+					<div className="main-content">
+						<Introduction 
+							fields={introduction}
+							formDisplay={formDisplay}
+						/>
+						<WorkExperience 
+							fields={workExperience}
+							formDisplay={formDisplay}
+						/>
+					</div>
+			</CVWrapper>
 		);
 	
 	}
