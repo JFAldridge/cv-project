1a5d9f8219c24ac3194e595b74cc6f2861dc1509
JFAldridge <jfaldridge1985@gmail.com>
2021-08-18 11:22:17 -0400

    Save schema.json to local whenever index is loaded

--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/cv.scss';
 
+import * as themes from './theme/schema.json';
+import { setToLS } from './utils/storage';
+
+const Index = () => {
+  setToLS('all-themes', themes.default);
+  return(
+    <App />
+  );
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Index />
   </React.StrictMode>,
   document.getElementById('root')
 );
