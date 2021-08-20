deeccc1db9768fbe4e29c86756bde7278d611216
JFAldridge <jfaldridge1985@gmail.com>
2021-08-20 10:59:12 -0400

    Add ThemeSelector

--- src/components/cv/form/theme-selector.jsx
+++ src/components/cv/form/theme-selector.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import { getFromLS } from '../../../utils/storage';
 import { useTheme } from '../../../theme/useTheme';
+import styled from 'styled-components';
+
+const ThemeBtn = styled.button`
+`;
+
+const ThemeIcon = styled.div`
+    width: 30px;
+    height: 30px;
+    background: ${props => `linear-gradient( 90deg, ${props.accentBg}, ${props.accentBg} 50%, ${props.bg} 51% )`};
+    border: 2px solid rgb(102, 102, 102); 
+    border-radius: 50%;
+`;
 
 function ThemeSelector({selectedThemeSet}) {
     const themesFromStore = getFromLS('all-themes');
@@ -14,12 +26,16 @@ function ThemeSelector({selectedThemeSet}) {
     return (
         Object.values(themesFromStore.data).map(theme => {
             return (
-                <button 
+                <ThemeBtn 
                     className="btn btn-dark me-3"
                     onClick={(selectedTheme) => setSelectedTheme(theme)}
                     key={theme.id}>
+                    <ThemeIcon 
+                        bg={theme.colors.bg}
+                        accentBg={theme.colors.accent.bg}
+                    />
                     {theme.name}
-                </button>
+                </ThemeBtn>
             );
         })
     );
