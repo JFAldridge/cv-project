b93dcc4658cdd23abc2e7aa241eb5d0ed2ace9b0
JFAldridge <jfaldridge1985@gmail.com>
2021-08-23 15:30:42 -0400

    Change useTheme to setMode on load if no localtheme

--- src/theme/useTheme.jsx
+++ src/theme/useTheme.jsx
@@ -19,7 +19,7 @@ export const useTheme = () => {
 
     useEffect(() => {
         const localTheme = getFromLS('theme');
-        localTheme ? setTheme(localTheme) : setTheme(themes.data.navyAvi);
+        localTheme ? setTheme(localTheme) : setMode(themes.data.navyAvi);
         setThemeLoaded(true);
     }, []);
 
